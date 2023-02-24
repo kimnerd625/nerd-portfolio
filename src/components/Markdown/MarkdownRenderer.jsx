e6e3d0562156ec0a84197d83d42bb5f24d87eaa9
kimnerd625 <jihunkim625@yonsei.ac.kr>
2023-02-24 18:31:15 +0900

    feat: md 파일 추가 및 테스트
    feat: md 파일 추가 및 테스트

--- src/components/Markdown/MarkdownRenderer.jsx
+++ src/components/Markdown/MarkdownRenderer.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MarkdownContainer } from './MarkdownRenderer.styles';
 import ReactMarkdown from "react-markdown";
 import introduction from './../../assets/markdown/introduction.md';
 
 const MarkdownRenderer = () => {
+  const [ markdown, setMarkdown ] = useState("");
+
+  useEffect(()=> {
+    fetch(introduction)
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
+  console.log(markdown)
+
   return (
     <MarkdownContainer>
+      <ReactMarkdown children={markdown}/>
+        {markdown}
       <ReactMarkdown>
-        {introduction}
+        # Hello, *World*!
       </ReactMarkdown>
     </MarkdownContainer>
   )
