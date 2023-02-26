d64c94473b74edb030d07f7c5afc1ef17e3e2266
kimnerd625 <jihunkim625@yonsei.ac.kr>
2023-02-26 23:54:27 +0900

    feat: 마크다운 정규표현식 추가
    feat: 마크다운 정규표현식 추가

--- src/components/Markdown/MarkdownReplacer.jsx
+++ src/components/Markdown/MarkdownReplacer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const MarkdownReplacer = (text) => { 
-  const newText = text.replace(/(\*\*)(?=\S)([^\r]*?\S[*_]*)\1/g, '<strong>$2</strong>')
+  const newText = text
+    .replace(/(\+R\+)(?=\S)([^\r]*?\S[*_]*)\1/g, '<boldred>$2</boldred>')
+    .replace(/(\+B\+)(?=\S)([^\r]*?\S[*_]*)\1/g, '<boldblue>$2</boldblue>')
+    .replace(/(\+G\+)(?=\S)([^\r]*?\S[*_]*)\1/g, '<boldgreen>$2</boldgreen>')
+    .replace(/(\*\*)(?=\S)([^\r]*?\S[*_]*)\1/g, '<strong>$2</strong>');
   console.log(newText)
   return newText;
 }
