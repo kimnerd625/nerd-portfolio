c0056fc0c3de73cec6aeb47d42372d89cde752ab
kimnerd625 <jihunkim625@yonsei.ac.kr>
2023-02-27 00:50:21 +0900

    feat: 마크다운 h1, h2, h3 정규표현식 구현
    feat: 마크다운 h1, h2, h3 정규표현식 구현

--- src/components/Markdown/MarkdownReplacer.jsx
+++ src/components/Markdown/MarkdownReplacer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const MarkdownReplacer = (text) => { 
+const MarkdownReplacer = (text) => {
+
   const newText = text
-    .replace(/(\+R\+)(?=\S)([^\r]*?\S[*_]*)\1/g, '<boldred>$2</boldred>')
-    .replace(/(\+B\+)(?=\S)([^\r]*?\S[*_]*)\1/g, '<boldblue>$2</boldblue>')
-    .replace(/(\+G\+)(?=\S)([^\r]*?\S[*_]*)\1/g, '<boldgreen>$2</boldgreen>')
-    .replace(/(\*\*)(?=\S)([^\r]*?\S[*_]*)\1/g, '<strong>$2</strong>')
-    .replace(/^\#.$/g, '<h1>$2</h1>');
+    .replace(/(?<!\#)(\#{1}?\s)(.*)/g, '<h1>$2</h1>')
+    .replace(/(?<!\#)(\#{2}?\s)(.*)/g, '<h2>$2</h2>')
+    .replace(/(?<!\#)(\#{3}?\s)(.*)/g, '<h3>$2</h3>')
+    .replace(/(\*\*)(?=\S)([^\r]*?\S[*_]*)\1/g, '<strong>$2</strong>');
   console.log(newText)
   return newText;
 }
