0856150d088ebce1c0d5e194706332350a78eaac
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-02-27 16:40:37 +0900

    feat: Timeline 컴포넌트 서식 구현
    feat: Timeline 컴포넌트 서식 구현

--- src/components/MyTimeline/MyTimelineCard.styles.jsx
+++ src/components/MyTimeline/MyTimelineCard.styles.jsx
@@ -1,12 +1,19 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const CardContainer = styled.div`
-  margin: 0 1rem;
-  display: flex;
-  flex-direction: row;
-`;
+const blink = keyframes`
+  0% {
+    background-color: #f7e0c1;
+  }
+  50% {
+    background-color: #5f7783;
+  }
+  100% {
+    background-color: #f7e0c1;
+  }
+`
 
 const CardShapeContainer = styled.div`
+  margin-left: 1rem;
   position: relative;
   border-left: 3px solid ${(props) => props.theme.color.lineColor};
 
@@ -20,27 +27,45 @@ const CardShapeContainer = styled.div`
     height: 1rem;
     background-color: ${(props) => props.theme.color.buttonColor};
     border-radius: 10rem;
-  } 
+  }
 `;
 
 const CardDetailContainer = styled.div`
-  margin: 1rem;
+margin: 1rem 1.5rem;
 `;
 
 const CardDate = styled.div`
-  margin: 0.75rem 0;
-  padding: 0.25rem 0.5rem;
-  border-radius: 0.5rem;
-  background-color: ${(props) => props.theme.color.buttonColor};
-  color: #fff;
+margin: 0.75rem 0;
+padding: 0.25rem 0.5rem;
+border-radius: 0.5rem;
+background-color: ${(props) => props.theme.color.buttonColor};
+color: #fff;
 `;
 
 const CardTitle = styled.div`
-
+  font-family: 'NanumSquareEB';
+  color: ${(props) => props.theme.color.headerColor_100};
 `;
-
+  
 const CardDescription = styled.div`
+  font-family: 'NanumSquareB';
+  color: ${(props) => props.theme.color.headerColor_200};
+`;
+  
+const CardContainer = styled.div`
+  margin: 0 1rem;
+  display: flex;
+  flex-direction: row;
 
+  &:hover ${CardShapeContainer}{  
+    &::after {
+      animation: ${blink} 1s linear infinite;
+    }
+  }
+
+  &:hover ${CardDate}{
+    background-color: ${(props) => props.theme.color.highlightColor_100};
+  }
 `;
 
-export { CardContainer, CardShapeContainer, CardDetailContainer, CardDate, CardTitle, CardDescription };
+  export { CardContainer, CardShapeContainer, CardDetailContainer, CardDate, CardTitle, CardDescription };
